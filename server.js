a219a349fe4cdd660702bf69a581acbbab95e5cf
AbdulRahman Alam <abdulrahmanidrees@gmail.com>
2024-11-25 20:50:02 +0300

    Exercise 1-4  is Done

--- server.js
+++ server.js
@@ -8,10 +8,6 @@ app.get('/greetings/:username', (req, res) => {
   res.send(`Hello there, ${username}!`)
 })
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000')
-})
-
 // 2. Rolling the Dice
 app.get('/roll/:number', (req, res) => {
   const number = parseInt(req.params.number, 10)
@@ -22,4 +18,65 @@ app.get('/roll/:number', (req, res) => {
     const roll = Math.floor(Math.random() * (number + 1))
     res.send(`You rolled a ${roll}.`)
   }
+})
+
+// 3. I Want THAT One!
+const collectibles = [
+  { name: 'shiny ball', price: 5.95 },
+  { name: 'autographed picture of a dog', price: 10 },
+  { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+]
+
+app.get('/collectibles/:index', (req, res) => {
+  const index = parseInt(req.params.index, 10)
+
+  if (index < 0 || index >= collectibles.length || isNaN(index)) {
+    res.send('This item is not yet in stock. Check back soon!')
+  } else {
+    const item = collectibles[index]
+    res.send(
+      `So, you want the ${item.name}? For ${item.price}, it can be yours!`
+    )
+  }
+})
+
+// 4. Filter Shoes by Query Parameters
+const shoes = [
+  { name: 'Birkenstocks', price: 50, type: 'sandal' },
+  { name: 'Air Jordans', price: 500, type: 'sneaker' },
+  { name: 'Air Mahomeses', price: 501, type: 'sneaker' },
+  { name: 'Utility Boots', price: 20, type: 'boot' },
+  { name: 'Velcro Sandals', price: 15, type: 'sandal' },
+  { name: 'Jet Boots', price: 1000, type: 'boot' },
+  { name: 'Fifty-Inch Heels', price: 175, type: 'heel' }
+]
+
+app.get('/shoes', (req, res) => {
+  const { 'min-price': minPrice, 'max-price': maxPrice, type } = req.query
+  let filteredShoes = shoes
+
+  if (minPrice) {
+    filteredShoes = filteredShoes.filter(
+      (shoe) => shoe.price >= parseFloat(minPrice)
+    )
+  }
+
+  if (maxPrice) {
+    filteredShoes = filteredShoes.filter(
+      (shoe) => shoe.price <= parseFloat(maxPrice)
+    )
+  }
+
+  if (type) {
+    filteredShoes = filteredShoes.filter(
+      (shoe) => shoe.type.toLowerCase() === type.toLowerCase().trim()
+    )
+  }
+
+  res.json(filteredShoes)
+})
+
+// Server Listener
+app.listen(3000, () => {
+  console.log('Server is running on port 3000')
 })
